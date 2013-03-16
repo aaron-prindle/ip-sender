674e8e8a03e7ec2494f4e0b00c5832803e0a13b5
Aaron Prindle <aaron@aaron-OptiPlex-745.(none)>
2013-03-16 19:31:16 -0400

    added email only on old ip changing

--- server-mail.py
+++ server-mail.py
@@ -3,37 +3,51 @@ import smtplib
 import socket
 import os
 import netrc
+import urllib2
 
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587
- 
-sender = 'aaprindle@gmail.com'
-recipient = 'aaprindle@gmail.com'
-# Read from the .netrc file in your home directory
-secrets = netrc.netrc()
-username, account, password = secrets.authenticators( SMTP_SERVER )
-subject = socket.gethostname() + " ** IP ADDRESS"
+def server_mail():
 
+    body = urllib2.urlopen("http://www.icanhazip.com").read()
+    with open('.old-ip', 'a+') as f:
+        old_ip = f.read()
+        f.close()
+        if body == old_ip:
+            return
+        else:
+            open('.old-ip', 'w').close()
+            w = open('.old-ip', 'w')
+            w.write(body)
+            
+            
+        SMTP_SERVER = 'smtp.gmail.com'
+        SMTP_PORT = 587
+        
+        sender = 'aaprindle@gmail.com'
+        recipient = 'aaprindle@gmail.com'
+        # Read from the .netrc file in your home directory
+        secrets = netrc.netrc()
+        username, account, password = secrets.authenticators( SMTP_SERVER )
+        subject = socket.gethostname() + " ** IP ADDRESS"
+            
+        "Sends an e-mail to the specified recipient."
+            
+        body = "" + body + ""
+            
+        headers = ["From: " + sender,
+                   "Subject: " + subject,
+                   "To: " + recipient,
+                   "MIME-Version: 1.0",
+                   "Content-Type: text/html"]
+        headers = "\r\n".join(headers)
+        
+        session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        
+        session.ehlo()
+        session.starttls()
+        session.ehlo
+        session.login(sender, password.decode('base64')) #or just password
+            
+        session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
+        session.quit()
 
-body = ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1])[0]
- 
-"Sends an e-mail to the specified recipient."
- 
-body = "" + body + ""
- 
-headers = ["From: " + sender,
-           "Subject: " + subject,
-           "To: " + recipient,
-           "MIME-Version: 1.0",
-           "Content-Type: text/html"]
-headers = "\r\n".join(headers)
- 
-session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
- 
-session.ehlo()
-session.starttls()
-session.ehlo
-session.login(sender, password.decode('base64')) #or just password
- 
-session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
-session.quit()
+server_mail()
